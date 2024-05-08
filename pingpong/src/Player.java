5e916dab79b68aa569124b5b6ac1ba3c42073c35
kkeevviinn123 <monteirokevin735@gmail.com>
2024-05-08 13:44:26 -0300

    Update Player.java

--- pingpong/src/Player.java
+++ pingpong/src/Player.java
@@ -1,88 +1,41 @@
-package PingPong;
+package PingPong.src;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
+// Importações necessárias
 import java.lang.String;
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Frame;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
+// Importando o pacote PingPong
+import PingPong.*;
 
+// Definição da classe Player
 public class Player {
 
-    private String name;
-    private int score;
-    private int matches_won;
+    // Variáveis de instância
+    private int score; // Pontuação do jogador
+    private int matches_won; // Número de partidas ganhas pelo jogador (ainda não utilizado)
 
+    // Dimensões do jogador
     private int width = 20;
     private int height = 200;
 
+    // Direção e velocidade do jogador
     private int direction = 0;
     private int velocity = 7;
-    
+
+    // Posição do jogador
     private int x;
     private int y;
 
+    // Cor do jogador
     private Color color = Color.white;
 
+    // Contador estático para acompanhar instâncias da classe Player
     private static int count = 0;
 
-    
+    // Construtor da classe Player
     public Player(int x, int y){
         this.x = x;
         this.y = y;
-        this.score = 0;
+        this.score = 0; // Inicializando a pontuação como zero
     }
-
-    public int getY(){
-        return this.y;
-    }
-
-    public void setDirection(int direction){
-        this.direction = direction * this.velocity;
-    }
-
-    public int getDirection() {
-        return this.direction;
-    }
-
-    public int getWidth(){
-        return this.width;
-    }
-
-    public int getHeight(){
-        return this.height;
-    }
-
-    public int getScore(){
-        return this.score;
-    }
-
-    public void incrementScore(){
-        this.score++;
-    }
-
-    public void draw(Graphics g){
-        g.setColor(this.color);
-        g.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-    public void updatePosition(){       
-        if((this.y + this.direction) < 0)
-            this.y = 0;
-        else if((this.y + this.direction) > (Game.height - this.height))
-            this.y = (Game.height - this.height);
-        else
-            this.y = this.y + this.direction;
-
-    }
-
-    
-
-}
